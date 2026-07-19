4014c15750083a5ffbae0c6238c017c08b4705e3
rahaf <rahaf@RAHAFSK>
2026-07-19 14:39:23 +0400

    class Rating

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Rating.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Rating.cs
@@ -1,6 +1,38 @@
-﻿namespace OmanCommunityServicesPlatform.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OmanCommunityServicesPlatform.Models
 {
     public class Rating
     {
+
+        [Key]
+        public int ratingId { get; set; }
+
+        [Required]
+        public int issueId { get; set; }
+
+        [Required]
+        public int userId { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int score { get; set; }
+
+        [StringLength(500)]
+        public string? feedback { get; set; }
+
+        [Required]
+        public DateTime ratedAt { get; set; }
+            = DateTime.UtcNow;
+
+
+        // Navigation Properties
+
+        [ForeignKey(nameof(issueId))]
+        public Issue Issue { get; set; } = null!;
+
+        [ForeignKey(nameof(userId))]
+        public User User { get; set; } = null!;
     }
 }
