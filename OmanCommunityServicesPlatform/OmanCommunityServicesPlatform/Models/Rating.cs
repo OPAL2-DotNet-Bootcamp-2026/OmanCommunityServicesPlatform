a76041e4f73020892fae041b5bbad57a3fc9e425
rahaf <rahaf@RAHAFSK>
2026-07-19 14:05:00 +0400

    prepare class Rating

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Rating.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Rating.cs
@@ -1,4 +1,7 @@
-﻿namespace OmanCommunityServicesPlatform.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OmanCommunityServices.Models
 {
     public class Rating
     {
