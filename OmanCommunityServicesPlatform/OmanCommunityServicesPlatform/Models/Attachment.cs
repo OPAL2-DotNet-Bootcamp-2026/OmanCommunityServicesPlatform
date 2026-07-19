82becc8e53f3afdd7b9622fa92f61ffebfa115b3
rahaf <rahaf@RAHAFSK>
2026-07-19 17:55:55 +0400

    Update Attachment Model

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Attachment.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Attachment.cs
@@ -1,8 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OmanCommunityServicesPlatform.Models
 {
+    [Table("Attachments")]
+    [Index(nameof(issueId), nameof(fileUrl), IsUnique = true)]
     public class Attachment
     {
 
@@ -10,11 +13,14 @@ namespace OmanCommunityServicesPlatform.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Auto-incrementing primary key
         public int attachmentId { get; set; }   // system generated - Primary key for the Attachment entity
 
-        [Required]
+        [ForeignKey(nameof(issue))] // Establishes a relationship with the Issue entity based on the issueId foreign key
         public int issueId { get; set; } // Foreign key - referencing the associated Issue entity
+        public Issue issue { get; set; }  // Navigation property to access the associated Issue entity
 
-        [Required]
+        [ForeignKey(nameof(uploadedBy))] // Establishes a relationship with the user entity based on the uploadedById foreign key 
         public int uploadedById { get; set; }  // Foreign key - referencing the User who uploaded the attachment
+        public User uploadedBy { get; set; } = null!; // Navigation property to access the User entity who uploaded the attachment
+
 
         [Required]
         [StringLength(300)] // Maximum length constraint for the file URL
@@ -26,14 +32,6 @@ namespace OmanCommunityServicesPlatform.Models
         [Required]
         public DateTime uploadedAt { get; set; } = DateTime.UtcNow; // system generated - Timestamp indicating when the attachment was uploaded, defaulting to the current UTC time
 
-
-        // Navigation Properties
-
-        [ForeignKey(nameof(issueId))] // Establishes a relationship with the Issue entity based on the issueId foreign key
-        public Issue Issue { get; set; }  // Navigation property to access the associated Issue entity
-
-        [ForeignKey(nameof(uploadedById))] // Establishes a relationship with the user entity based on the uploadedById foreign key 
-        public User UploadedBy { get; set; } = null!; // Navigation property to access the User entity who uploaded the attachment
     }
 }
         
