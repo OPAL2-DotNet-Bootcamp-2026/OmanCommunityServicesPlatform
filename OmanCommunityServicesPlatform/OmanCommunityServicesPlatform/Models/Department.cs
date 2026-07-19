943a04b558e754b9568a7f259c8ee3f4d1150e7e
Hidaya21 <alabrihidaya@gmail.com>
2026-07-19 12:04:39 +0400

    Department Model Done

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Department.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Department.cs
@@ -1,6 +1,25 @@
-﻿namespace OmanCommunityServicesPlatform.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OmanCommunityServicesPlatform.Models
 {
+    [Table("Departments")]
     public class Department
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int departmentId { get; set; }           // system generated
+        [Required]
+        [MaxLength(100)]
+        public string departmentName { get; set; }       // user input
+        [MaxLength(500)]
+        public string? description { get; set; }        // user input (optional)
+        [Required]
+        [MaxLength(150)]
+        [EmailAddress]
+        public string contactEmail { get; set; }        // user input
+        [ForeignKey("region")]
+        public int? regionId { get; set; }             // from list 
+        public virtual Region? region { get; set; }    // navigation property
     }
 }
