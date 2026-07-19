b303679df28b8d696a346cc0cc391be6d6825fed
rahaf <rahaf@RAHAFSK>
2026-07-19 16:31:44 +0400

    source tags

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Rating.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Rating.cs
@@ -8,23 +8,23 @@ namespace OmanCommunityServicesPlatform.Models
 
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // This will make the ratingId auto-increment
-        public int ratingId { get; set; } // Primary key for the Rating entity
+        public int ratingId { get; set; } //System generated - Primary key for the Rating entity
 
         [Required]
-        public int issueId { get; set; } // Foreign key referencing the Issue entity
+        public int issueId { get; set; } // user input - Foreign key referencing the Issue entity
 
         [Required]
-        public int userId { get; set; } // Foreign key referencing the User entity
+        public int userId { get; set; } // user input - Foreign key referencing the User entity
 
         [Required]
         [Range(1, 5)] // Validation to ensure the score is between 1 and 5
-        public int score { get; set; } // The rating score given by the user
+        public int score { get; set; } //user input - The rating score given by the user
 
         [StringLength(500)] // Validation to limit the feedback length to 500 characters
-        public string? feedback { get; set; } // Optional feedback provided by the user about the issue     
+        public string? feedback { get; set; } // user input - Optional feedback provided by the user about the issue     
 
         [Required]
-        public DateTime ratedAt { get; set; } // The date and time when the rating was created
+        public DateTime ratedAt { get; set; } // System generated - The date and time when the rating was created
             = DateTime.UtcNow; // Default value set to the current UTC date and time when a new rating is created
 
 
