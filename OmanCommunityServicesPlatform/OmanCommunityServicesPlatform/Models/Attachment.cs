38383fc664ed739c828fa5bf56b669e3e2b71fbb
rahaf <rahaf@RAHAFSK>
2026-07-19 17:40:13 +0400

    Revert Attachment

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Attachment.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Attachment.cs
@@ -1,39 +1,7 @@
-﻿using OmanCommunityServicesPlatform.Models;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace OmanCommunityServices.Models
+﻿namespace OmanCommunityServicesPlatform.Models
 {
     public class Attachment
     {
-        [Key]
-        public int attachmentId { get; set; }
 
-        [Required]
-        public int issueId { get; set; }
-
-        [Required]
-        public int uploadedById { get; set; }
-
-        [Required]
-        [StringLength(300)]
-        public string fileUrl { get; set; } = string.Empty;
-
-        [Required]
-        public string fileType { get; set; }
-
-        [Required]
-        public DateTime uploadedAt { get; set; }
-            = DateTime.UtcNow;
-
-
-        // Navigation Properties
-
-        [ForeignKey(nameof(ssueId))]
-        public Issue Issue { get; set; } = null!;
-
-        [ForeignKey(nameof(uploadedById))]
-        public User UploadedBy { get; set; } = null!;
     }
 }
-        
