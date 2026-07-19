195ba0922ffc230141bde1fdcf300fa2c43c47b7
rahaf <rahaf@RAHAFSK>
2026-07-19 12:19:17 +0400

    class Attachment

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Attachment.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Attachment.cs
@@ -7,6 +7,34 @@ namespace OmanCommunityServices.Models
     public class Attachment
     {
 
+        [Key]
+        public int attachmentId { get; set; }
+
+        [Required]
+        public int issueId { get; set; }
+
+        [Required]
+        public int uploadedById { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string fileUrl { get; set; } = string.Empty;
+
+        [Required]
+        public string fileType { get; set; }
+
+        [Required]
+        public DateTime uploadedAt { get; set; }
+            = DateTime.UtcNow;
+
+
+        // Navigation Properties
+
+        [ForeignKey(nameof(issueId))]
+        public Issue Issue { get; set; } = null!;
+
+        [ForeignKey(nameof(uploadedById))]
+        public User UploadedBy { get; set; } = null!;
     }
 }
         
