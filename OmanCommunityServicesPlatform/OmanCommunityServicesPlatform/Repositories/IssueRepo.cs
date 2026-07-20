4fde5ea4816bd914f6eecc767c3662550aa08fd2
wajdan <wejdan95.almamari@gmail.com>
2026-07-20 09:42:44 +0400

    Implement Issue repository

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/IssueRepo.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/IssueRepo.cs
@@ -1,8 +1,43 @@
-﻿namespace OmanCommunityServicesPlatform.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using OmanCommunityServicesPlatform.Models;
+
+namespace OmanCommunityServicesPlatform.Repositories
 {
     public class IssueRepo
     {
-
-
+        private OCSPContext context;
+        public IssueRepo(OCSPContext context) 
+        { 
+            this.context = context;
+        }
+        public List<Issue> GetAll()
+        {
+            return context.Issues.ToList();
+        }
+        public Issue GetById(int issueId)
+        {
+            return context.Issues.FirstOrDefault(i => i.issueId == issueId);
+        }
+        public Issue GetWithDetails(int issueId)
+        {
+            return context.Issues
+                .Include(i => i.reportedBy)
+                .Include(i => i.category)
+                .Include(i => i.region)
+                .Include(i => i.assignedDepartment)
+                .Include(i => i.comments)
+                .Include(i => i.statusUpdates)
+                .FirstOrDefault(i => i.issueId == issueId);
+        }
+        public void Add(Issue issue)
+        {
+            context.Issues.Add(issue);
+            context.SaveChanges();
+        }
+        public void Delete(Issue issue)
+        {
+            context.Issues.Remove(issue);
+            context.SaveChanges();
+        }
     }
 }
