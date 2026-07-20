c6c753e84f5365bed7ddb62c19489fcb79d8ecc4
rahaf <rahaf@RAHAFSK>
2026-07-20 16:28:09 +0400

    new update for NotificationDTOs

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/NotificationDTOs.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/NotificationDTOs.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using OmanCommunityServicesPlatform.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace OmanCommunityServicesPlatform
 {
@@ -6,10 +7,6 @@ namespace OmanCommunityServicesPlatform
     // Used when creating a new notification
     public class CreateNotificationDTO
     {
-        [Required]
-        public int userId { get; set; }
-        // Foreign key referencing the user who will receive the notification
-
         public int? issueId { get; set; }
         // Optional foreign key referencing the related issue
 
@@ -18,15 +15,12 @@ namespace OmanCommunityServicesPlatform
             300,
             ErrorMessage = "Message cannot exceed 300 characters."
         )]
-        public string message { get; set; } = string.Empty;
+        public string message { get; set; }
         // Notification message
 
-        [Required]
-        [StringLength(
-            30,
-            ErrorMessage = "Notification type cannot exceed 30 characters."
-        )]
-        public string type { get; set; } = string.Empty;
+        [Required(ErrorMessage ="Notification Type is Required")]
+   
+        public NotificationType type { get; set; } 
         // Notification type such as StatusChange, Comment, or Assignment
     }
 
