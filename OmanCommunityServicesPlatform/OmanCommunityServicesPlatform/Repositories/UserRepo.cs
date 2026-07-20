fb58253aa43951f09a1a62c67a98db4297d5eaf4
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-20 13:29:16 +0400

    Add user GetByDepartment method and comments to UserRepo

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/UserRepo.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/UserRepo.cs
@@ -11,25 +11,35 @@ namespace OmanCommunityServicesPlatform.Repositories
             context = _context;
         }
 
+        // Needed for Registration
         public void Add(User user)
         {
             context.Users.Add(user);
             context.SaveChanges();
         }
 
+        // Needed for Admin's user management screen
         public List<User> GetAll()
         {
             return context.Users.ToList();
         }
 
+        // 	Needed for profile views, and internally by Service for almost every other operation
         public User GetById(int id)
         {
             return context.Users.FirstOrDefault(u => u.userId == id);
         }
 
+        // Needed for login lookups and for checking duplicate registrations
         public User GetByEmail(string email)
         {
             return context.Users.FirstOrDefault(u => u.email ==email);
+        }
+
+        // Needed so Staff dashboards or Admin can see who's in a department
+        public List<User> GetByDepartment(int departmentId)
+        {
+            return context.Users.Where(u => u.departmentId == departmentId).ToList();
         }
 
         public void Update()
