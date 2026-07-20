4ffb796fc65418128f3027c0d6f782baa1346ac9
rahaf <rahaf@RAHAFSK>
2026-07-20 16:31:11 +0400

    Update the RaitingDTOs

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/RatingDTOs.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/RatingDTOs.cs
@@ -11,22 +11,11 @@ namespace OmanCommunityServicesPlatform
         public int issueId { get; set; }
         // Foreign key referencing the issue being rated
 
-        [Required]
-        public int userId { get; set; }
-        // Foreign key referencing the user who submits the rating
-
-        [Required]
-        [Range(
-            1,
-            5,
-            ErrorMessage = "Score must be between 1 and 5."
-        )]
+        [Required (ErrorMessage = "Score must be between 1 and 5.")]
+    
         public int score { get; set; }
         // User input
-        [StringLength(
-        500,
-        ErrorMessage = "Feedback cannot exceed 500 characters."
-    )]
+       
         public string? feedback { get; set; }
         // Optional user input
     }
@@ -34,20 +23,11 @@ namespace OmanCommunityServicesPlatform
     // Used when updating an existing rating
     public class UpdateRatingDto
     {
-        [Required]
-        [Range(
-        1,
-        5,
-        ErrorMessage = "Score must be between 1 and 5."
-    )]
+        [Required(ErrorMessage = "Score must be between 1 and 5.")]
 
         public int score { get; set; }
         // Updated score from the user
 
-        [StringLength(
-        500,
-        ErrorMessage = "Feedback cannot exceed 500 characters."
-            )]
         public string? feedback { get; set; }
         // Updated optional feedback
     }     
