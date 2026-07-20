4d3e8d5aeda02f31d8a89471a9a09418d2b57b69
rahaf <rahaf@RAHAFSK>
2026-07-20 09:51:33 +0400

    notes for  class Notification Model

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Notification.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Notification.cs
@@ -17,29 +17,29 @@ namespace OmanCommunityServicesPlatform.Models
 
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int notificationId { get; set; } 
+        public int notificationId { get; set; } // System generated
 
         [ForeignKey(nameof(user))]
-        public int userId { get; set; } 
+        public int userId { get; set; } // Foreign Key from User
         public User user { get; set; }
 
         [ForeignKey(nameof(issue))]
-        public int? issueId { get; set; } 
+        public int? issueId { get; set; } // Optional Foreign Key from Issue  
         public Issue? issue { get; set; }
 
         [Required]
         [StringLength(300)]
-        public string message { get; set; } 
+        public string message { get; set; } // System generated
 
         [Required]
         [StringLength(30)]
-        public string type { get; set; } 
+        public string type { get; set; } // System generated
 
-        public bool isRead { get; set; } = false; 
+        public bool isRead { get; set; } = false; // Default value set to false, indicating the notification is unread when created
 
         [Required]
-        public DateTime createdAt { get; set; } = DateTime.UtcNow;
-        
+        public DateTime createdAt { get; set; } = DateTime.UtcNow; // Default value set to the current UTC date and time when a new notification is created
+
 
     }
 }
