d63d96368460b802046b0dc2a9fb9b221475c365
rahaf <rahaf@RAHAFSK>
2026-07-20 14:26:45 +0400

    class NotificationDTO

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/NotificationDTOs.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/NotificationDTOs.cs
@@ -56,5 +56,35 @@ namespace OmanCommunityServicesPlatform
             public bool isRead { get; set; }
         }
 
+        // Used to return notification information from the API
+        public class NotificationDTO
+        {
+            public int notificationId { get; set; }
+            // System-generated notification ID
+
+            public int userId { get; set; }
+            // Foreign key of the user receiving the notification
+
+            public string userName { get; set; } = string.Empty;
+            // Name of the user receiving the notification
+
+            public int? issueId { get; set; }
+            // Optional foreign key of the related issue
+
+            public string? issueTitle { get; set; }
+            // Optional title of the related issue
+
+            public string message { get; set; } = string.Empty;
+            // Notification message
+
+            public string type { get; set; } = string.Empty;
+            // Notification type
+
+            public bool isRead { get; set; }
+            // Shows whether the notification has been read
+
+            public DateTime createdAt { get; set; }
+            // Date and time when the notification was created
+        }
     }
 }
