60c750afc945a9240fa1c13a2ee5254bc61e6268
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-20 10:29:23 +0400

    Add UserRepo class with basic CRUD operations

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/UserRepo.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/UserRepo.cs
@@ -1,6 +1,32 @@
-﻿namespace OmanCommunityServicesPlatform.Repositories
+﻿using OmanCommunityServicesPlatform.Models;
+
+namespace OmanCommunityServicesPlatform.Repositories
 {
     public class UserRepo
     {
+        private OCSPContext context;
+        
+        public UserRepo (OCSPContext _context)
+        {
+            context = _context;
+        }
+
+        public void Add(User user)
+        {
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+
+        public void Update()
+        {
+            context.SaveChanges();
+        }
+
+        public User GetById(int id)
+        {
+            return context.Users.FirstOrDefault(u => u.userId == id);
+        }
+
+
     }
 }
