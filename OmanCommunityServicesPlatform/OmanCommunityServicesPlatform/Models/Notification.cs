b45e5979210f6bd1c7643f7bc0ca47b10a0f5b31
rahaf <rahaf@RAHAFSK>
2026-07-20 09:49:16 +0400

    class Notification

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Notification.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Models/Notification.cs
@@ -4,9 +4,42 @@ using Microsoft.EntityFrameworkCore;
 
 namespace OmanCommunityServicesPlatform.Models
 {
+    [Table("Notifications")]
+    [Index(
+      nameof(userId),
+      nameof(issueId),
+      nameof(type),
+      nameof(message),
+      IsUnique = true
+  )]
     public class Notification
     {
-       
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int notificationId { get; set; } 
+
+        [ForeignKey(nameof(user))]
+        public int userId { get; set; } 
+        public User user { get; set; }
+
+        [ForeignKey(nameof(issue))]
+        public int? issueId { get; set; } 
+        public Issue? issue { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string message { get; set; } 
+
+        [Required]
+        [StringLength(30)]
+        public string type { get; set; } 
+
+        public bool isRead { get; set; } = false; 
+
+        [Required]
+        public DateTime createdAt { get; set; } = DateTime.UtcNow;
+        
 
     }
 }
