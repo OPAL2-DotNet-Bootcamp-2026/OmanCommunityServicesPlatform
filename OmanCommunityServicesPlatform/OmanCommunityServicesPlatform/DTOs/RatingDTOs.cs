945d0f2b31a4b4c4259465153d0581a65e417d8e
rahaf <rahaf@RAHAFSK>
2026-07-20 11:47:11 +0400

    Create class CreateRatingDto

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/RatingDTOs.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/RatingDTOs.cs
@@ -1,6 +1,41 @@
-﻿namespace OmanCommunityServicesPlatform.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OmanCommunityServicesPlatform.DTOs
 {
+    // Used to return rating information from the API
+
     public class RatingDTOs
     {
+        // Used when creating a new rating
+
+        public class CreateRatingDto
+        {
+            [Required]
+            public int issueId { get; set; }
+            // Foreign key referencing the issue being rated
+
+            [Required]
+            public int userId { get; set; }
+            // Foreign key referencing the user who submits the rating
+
+            [Required]
+            [Range(
+                1,
+                5,
+                ErrorMessage = "Score must be between 1 and 5."
+            )]
+            public int score { get; set; }
+            // User input
+            [StringLength(
+            500,
+            ErrorMessage = "Feedback cannot exceed 500 characters."
+        )]
+            public string? feedback { get; set; }
+            // Optional user input
+        }
+
     }
 }
+
+
+
