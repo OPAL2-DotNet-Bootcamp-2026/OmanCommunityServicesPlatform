ce8306c29423024aa627b49322c43cbefd1226a3
rahaf <rahaf@RAHAFSK>
2026-07-20 11:47:56 +0400

    Create  class UpdateRatingDto

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/RatingDTOs.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/RatingDTOs.cs
@@ -34,6 +34,28 @@ namespace OmanCommunityServicesPlatform.DTOs
             // Optional user input
         }
 
+        // Used when updating an existing rating
+        public class UpdateRatingDto
+        {
+            [Required]
+            [Range(
+            1,
+            5,
+            ErrorMessage = "Score must be between 1 and 5."
+        )]
+
+            public int score { get; set; }
+            // Updated score from the user
+
+            [StringLength(
+            500,
+            ErrorMessage = "Feedback cannot exceed 500 characters."
+                )]
+            public string? feedback { get; set; }
+            // Updated optional feedback
+        }
+
+
     }
 }
 
