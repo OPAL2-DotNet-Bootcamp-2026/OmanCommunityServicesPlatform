7cb61bd4dd6049eb6f895de954c0e2fd99e099a4
rahaf <rahaf@RAHAFSK>
2026-07-21 11:22:20 +0400

    add the using

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/NotificationRepo.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/NotificationRepo.cs
@@ -1,6 +1,11 @@
-﻿namespace OmanCommunityServicesPlatform.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using OmanCommunityServicesPlatform.Models;
+
+namespace OmanCommunityServicesPlatform
 {
+    // Handles database operations related to Notification.
     public class NotificationRepo
     {
+        
     }
 }
