08c6d0dca5c9cb0840deb72c01f70bba05def7fa
Hidaya21 <alabrihidaya@gmail.com>
2026-07-21 15:13:51 +0400

    CreateCategoryDTO Class

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/CategoryDTOs.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/CategoryDTOs.cs
@@ -1,6 +1,27 @@
-﻿namespace OmanCommunityServicesPlatform.DTOs
+﻿using OmanCommunityServicesPlatform.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OmanCommunityServicesPlatform.DTOs
 {
-    public class CategoryDTOs
+    public class RepoCategoryDTOs
     {
+        public int categoryId { get; set; }
+        public string categoryName { get; set; } = string.Empty;
+        public string? description { get; set; }
+        public int departmentId { get; set; }
+        public string? departmentName { get; set; }
+        public int issueCount { get; set; }
+    }
+    //Used when creating a new category 
+    public class CreateCategoryDTO
+    {
+        [Required]
+        [MaxLength(100)]
+        public string categoryName { get; set; } = string.Empty;
+        [MaxLength(300)]
+        public string? description { get; set; }
+        [Required]
+        public int departmentId { get; set; }
     }
 }
