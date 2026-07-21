a3399e3e4221b09521b5dfd5bfba4e535d96ade8
rahaf <rahaf@RAHAFSK>
2026-07-21 09:56:20 +0400

    first part of RatingRepo (DATA)

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/RatingRepo.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/RatingRepo.cs
@@ -1,6 +1,28 @@
-﻿namespace OmanCommunityServicesPlatform.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using OmanCommunityServicesPlatform.Models;
+
+
+namespace OmanCommunityServicesPlatform
+
 {
     public class RatingRepo
+
     {
+        // Stores the database context.
+        // readonly means it can only be assigned in the constructor.
+        private readonly OCSPContext context;
+
+
+        // Constructor Dependency Injection.
+        // ASP.NET Core sends CommunityServicesContext automatically
+        // when it creates RatingRepository.
+        public RatingRepo(OCSPContext context)
+        {
+            // this.context refers to the field above.
+            // context refers to the object received in the constructor.
+            this.context = context;
+        }
+
     }
 }
+
