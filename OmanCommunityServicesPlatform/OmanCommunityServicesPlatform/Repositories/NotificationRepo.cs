ec8f68965bade4c87605217d960769d0a46f79d8
rahaf <rahaf@RAHAFSK>
2026-07-21 12:07:47 +0400

    manage the context at NotificationRepo

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/NotificationRepo.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/NotificationRepo.cs
@@ -1,11 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OmanCommunityServicesPlatform.Models;
+using OmanCommunityServicesPlatform.Models.Enums;
 
 namespace OmanCommunityServicesPlatform
 {
-    // Handles database operations related to Notification.
     public class NotificationRepo
     {
-        
+        // Stores the database context.
+        // readonly means it can only be assigned in the constructor.
+        private readonly OCSPContext context;
+
+        // Constructor Dependency Injection.
+        // ASP.NET Core provides CommunityServicesContext automatically.
+        public NotificationRepo(OCSPContext context)
+        {
+            // Saves the received context inside the repository field.
+            this.context = context;
+        }
+
+  
+
     }
+    
 }
