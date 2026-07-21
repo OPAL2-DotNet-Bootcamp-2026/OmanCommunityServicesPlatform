e4e60866172b5e8a535f9bb6472eaf4f53641ab3
Hidaya21 <alabrihidaya@gmail.com>
2026-07-21 15:17:24 +0400

    Add UpdateCategoryDTO Class

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/CategoryDTOs.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/CategoryDTOs.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OmanCommunityServicesPlatform.DTOs
 {
-    public class RepoCategoryDTOs
+    public class RepoCategoryDTO
     {
         public int categoryId { get; set; }
         public string categoryName { get; set; } = string.Empty;
@@ -24,4 +24,17 @@ namespace OmanCommunityServicesPlatform.DTOs
         [Required]
         public int departmentId { get; set; }
     }
+
+    // Used when updating a category
+    public class UpdateCategoryDTO
+    {
+        [Required]
+        [MaxLength(100)]
+        public string categoryName { get; set; } = string.Empty;
+        [MaxLength(300)]
+        public string? description { get; set; }
+        [Required]
+        public int departmentId { get; set; }
+    }
+
 }
