c51bf66e8128062cc0f73828bacb9610a365b64a
Hidaya21 <alabrihidaya@gmail.com>
2026-07-22 09:36:14 +0400

    Add Include in Category Repositry

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/CategoryRepo.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/CategoryRepo.cs
@@ -13,12 +13,12 @@ namespace OmanCommunityServicesPlatform.Repositories
         // Get all categories
         public List<Category> GetAllCategories()
         {
-            return context.Categories.ToList();
+            return context.Categories.Include(c => c.department).Include(c => c.Issues).ToList();
         }
         // Get Category by id
         public Category? GetCategoryById(int id)
         {
-            return context.Categories.FirstOrDefault(c => c.categoryId == id);
+            return context.Categories.Include(c => c.department).Include(c => c.Issues).FirstOrDefault(c => c.categoryId == id);
         }
         // Add new category
         public void Add(Category category)
