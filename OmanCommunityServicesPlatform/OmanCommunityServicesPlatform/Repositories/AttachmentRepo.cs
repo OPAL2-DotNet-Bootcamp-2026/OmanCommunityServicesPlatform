992c3458a2878848eca932d761dd3f9fceca5164
wajdan <wejdan95.almamari@gmail.com>
2026-07-22 11:57:51 +0400

    Create Attachment repository

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/AttachmentRepo.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/AttachmentRepo.cs
@@ -2,5 +2,10 @@
 {
     public class AttachmentRepo
     {
+        private OCSPContext context;
+        public AttachmentRepo(OCSPContext _context)
+        {
+            context = _context;
+        }
     }
 }
