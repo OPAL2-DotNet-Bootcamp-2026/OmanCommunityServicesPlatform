5e6320a6a6f34371f92e75850c005b5f8ffbfc0e
rahaf <rahaf@RAHAFSK>
2026-07-22 11:35:10 +0400

    class RatingService - OCSPContext

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Services/RatingService.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Services/RatingService.cs
@@ -1,6 +1,26 @@
-﻿namespace OmanCommunityServicesPlatform.Services
+﻿using OmanCommunityServicesPlatform.DTOs;
+using OmanCommunityServicesPlatform.Models;
+
+namespace OmanCommunityServicesPlatform
 {
+    // The RatingService contains the business logic
+    // related to ratings.
+    //
+    // The controller calls the service.
+    // The service calls the repository.
     public class RatingService
     {
+        // Stores the Rating repository.
+        // The repository communicates with the database.
+        private readonly OCSPContext ratingRepo;
+
+
+        // Constructor Dependency Injection.
+        // ASP.NET Core provides RatingRepo automatically.
+        public RatingService(OCSPContext ratingRepo)
+        {
+            // Stores the received repository in the field.
+            this.ratingRepo = ratingRepo;
+        }
     }
 }
