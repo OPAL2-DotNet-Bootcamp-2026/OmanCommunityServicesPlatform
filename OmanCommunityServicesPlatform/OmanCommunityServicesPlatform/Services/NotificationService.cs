a960ff05c30bd5edb439f61ea946c2d3fe8864fe
rahaf <rahaf@RAHAFSK>
2026-07-22 22:08:25 +0400

    GET NOTIFICATIONS BY USER ID

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Services/NotificationService.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Services/NotificationService.cs
@@ -40,5 +40,31 @@ namespace OmanCommunityServicesPlatform.Services
         {
             return notificationRepo.GetById(notificationId);
         }
+
+        // --------------------------------------------------
+        // GET NOTIFICATIONS BY USER ID
+        // --------------------------------------------------
+
+        public List<Notification> GetNotificationsByUserId(
+            int userId
+        )
+        {
+            // Check whether the selected user exists.
+            bool userExists = context.Users.Any(user =>
+                user.userId == userId
+            );
+
+            // Stop the operation if the user does not exist.
+            if (!userExists)
+            {
+                throw new KeyNotFoundException(
+                    "User was not found."
+                );
+            }
+
+            return notificationRepo.GetByUserId(userId);
+        }
+
+
     }
 }
