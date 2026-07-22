ce65040b30b61e9dcc01e5a163ad9c433c664105
wajdan <wejdan95.almamari@gmail.com>
2026-07-22 11:34:37 +0400

    CreateAttachmentDto

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/AttachmentDTOs.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/DTOs/AttachmentDTOs.cs
@@ -1,6 +1,22 @@
-﻿namespace OmanCommunityServicesPlatform.DTOs
+﻿using OmanCommunityServicesPlatform.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace OmanCommunityServicesPlatform.DTOs
 {
-    public class AttachmentDTOs
+    // Data sent from the client to create a new attachment
+    public class CreateAttachmentDto
     {
+        [Required]
+        public int issueId { get; set; }
+
+        [Required]
+        public int uploadedById { get; set; }
+
+        [Required]
+        [StringLength(300)]
+        public string fileUrl { get; set; }
+
+        [Required]
+        public AttachmentFileType fileType { get; set; }
     }
 }
