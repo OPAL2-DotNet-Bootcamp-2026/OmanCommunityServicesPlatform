dc24ef2ee96c8079412dad18689ebb87b0afbff2
wajdan-Almamari <wejdan95.almamari@gmail.com>
2026-07-23 11:28:47 +0400

    Add AttachmentService

---
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Services/AttachmentService.cs
@@ -0,0 +1,56 @@
+﻿using OmanCommunityServicesPlatform.DTOs;
+using OmanCommunityServicesPlatform.Models;
+using OmanCommunityServicesPlatform.Repositories;
+
+namespace OmanCommunityServicesPlatform.Services
+{
+    public class AttachmentService
+    {
+        private AttachmentRepo attachmentRepo;
+        private IssueRepo issueRepo;
+        private UserRepo userRepo;
+
+        public AttachmentService(AttachmentRepo _attachmentRepo, IssueRepo _issueRepo, UserRepo _userRepo)
+        {
+            attachmentRepo = _attachmentRepo;
+            issueRepo = _issueRepo;
+            userRepo = _userRepo;
+        }
+        // Create attachment
+        public AttachmentResponseDto? Create(CreateAttachmentDto dto)
+        {
+            Issue? issue = issueRepo.GetById(dto.issueId);
+
+            if (issue == null)
+                return null;
+
+            User? user = userRepo.GetById(dto.uploadedById);
+
+            if (user == null)
+                return null;
+
+            Attachment? existingAttachment = attachmentRepo.GetByIssueIdAndUrl(dto.issueId,dto.fileUrl);
+            if (existingAttachment != null)
+                return null;
+
+            Attachment attachment = new Attachment();
+            attachment.issueId = dto.issueId;
+            attachment.uploadedById = dto.uploadedById;
+            attachment.fileUrl = dto.fileUrl;
+            attachment.fileType = dto.fileType;
+            attachment.uploadedAt = DateTime.UtcNow;
+
+            attachmentRepo.Add(attachment);
+            AttachmentResponseDto response =new AttachmentResponseDto();
+
+            response.attachmentId = attachment.attachmentId;
+            response.issueId = attachment.issueId;
+            response.uploadedById = attachment.uploadedById;
+            response.fileUrl = attachment.fileUrl;
+            response.fileType = attachment.fileType;
+            response.uploadedAt = attachment.uploadedAt;
+
+            return response;
+        }
+    }
+}
