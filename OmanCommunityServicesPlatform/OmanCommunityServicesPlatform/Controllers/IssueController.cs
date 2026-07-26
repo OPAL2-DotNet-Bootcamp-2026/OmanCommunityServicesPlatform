bbaaef390ca22bf8d2e59e95a47598414d48a873
wajdan <wejdan95.almamari@gmail.com>
2026-07-26 15:37:50 +0400

    CreateIssue

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Controllers/IssueController.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Controllers/IssueController.cs
@@ -1,6 +1,36 @@
-﻿namespace OmanCommunityServicesPlatform.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OmanCommunityServicesPlatform.DTOs;
+using OmanCommunityServicesPlatform.Services;
+
+namespace OmanCommunityServicesPlatform.Controllers
 {
-    public class IssueController
+    [ApiController]
+    [Route("issue")]
+    [Authorize]
+    public class IssueController : ControllerBase
     {
+        private IssueService issueService;
+        public IssueController(IssueService _issueService) 
+        {
+            issueService = _issueService;
+        }
+
+        // POST issue/CreateIssue
+        // Citizen reports a new issue
+        [HttpPost("CreateIssue")]
+        [Authorize]
+        public IActionResult create([FromRoute] int reportedById , [FromBody] CreateIssueDto dto)
+        {
+            IssueResponseDto created = issueService.Create(dto, reportedById);
+
+            if (created == null)
+            {
+                return BadRequest(new { message = "The selected category or region does not exist." });
+            }
+            return Ok(created); //200, issue created
+
+        }
+        
     }
 }
