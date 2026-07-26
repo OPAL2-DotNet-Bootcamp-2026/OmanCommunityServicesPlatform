6c8e60ec0eca78d943bf917b95b627e4248eac2a
rahaf <rahafalkiuymi.saeed@gmail.com>
2026-07-26 09:39:26 +0400

    RatingController : ControllerBase

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Controllers/RatingController.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Controllers/RatingController.cs
@@ -1,6 +1,23 @@
-﻿namespace OmanCommunityServicesPlatform.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using OmanCommunityServicesPlatform.DTOs;
+using OmanCommunityServicesPlatform.Services;
+
+namespace OmanCommunityServicesPlatform.Controllers
 {
-    public class RatingController
+    [ApiController]
+    [Route("api/rating")]
+    public class RatingController : ControllerBase
     {
+        // Service used for Rating business logic.
+        private readonly RatingService ratingService;
+
+
+        // Constructor Dependency Injection.
+        // ASP.NET Core provides RatingService automatically.
+        public RatingController(RatingService ratingService)
+        {
+            this.ratingService = ratingService;
+        }
+
     }
 }
