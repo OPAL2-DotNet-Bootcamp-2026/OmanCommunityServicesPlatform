c7acee4c503aff8360cb3ce12876decc0aeeb6f0
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-26 14:45:22 +0400

    Integrate JWT authentication and Swagger JWT support

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Program.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using OmanCommunityServicesPlatform.Repositories;
 using OmanCommunityServicesPlatform.Services;
+using System.Text;
 
 namespace OmanCommunityServicesPlatform
 {
@@ -21,11 +25,65 @@ namespace OmanCommunityServicesPlatform
             // Services
             builder.Services.AddScoped<UserService>();
 
+            // Register AuthService 
+            builder.Services.AddScoped<AuthService>();
+            // Read JWT settings from appsettings.json 
+            var jwtKey = builder.Configuration["JwtSettings: SecretKey"];
+            var jwtIssuer = builder.Configuration["JwtSettings: Issuer"];
+            var jwtAudience = builder.Configuration["JwtSettings: Audience"];
+            // Configure how incoming tokens are validated
+            builder.Services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true, // reject expired tokens
+                    ValidateIssuerSigningKey = true, // verify the signature
+                    ValidIssuer = jwtIssuer,
+                    ValidAudience = jwtAudience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtKey))
+                };
+            });
+            builder.Services.AddAuthorization();
+
+            // Register Controllers
             builder.Services.AddControllers();
 
             // Swagger
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            //builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "Enter your JWT token in the box below"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id   = "Bearer"
+                            }
+                        },
+                        new List<string>()
+                    }
+                });
+            });
 
             var app = builder.Build();
 
@@ -39,6 +97,8 @@ namespace OmanCommunityServicesPlatform
 
             app.UseHttpsRedirection();
 
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
 
