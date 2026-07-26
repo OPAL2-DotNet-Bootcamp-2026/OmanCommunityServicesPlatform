f616a9df1d7d5500b09032a943e0b2d3cc97920a
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-26 15:38:02 +0400

    Refactor UserController and add user registration endpoint

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Controllers/UserController.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Controllers/UserController.cs
@@ -1,6 +1,36 @@
-﻿namespace OmanCommunityServicesPlatform.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OmanCommunityServicesPlatform.DTOs;
+using OmanCommunityServicesPlatform.Services;
+
+namespace OmanCommunityServicesPlatform.Controllers
 {
-    public class UserController
+    [ApiController]
+    [Route("user")]
+    [Authorize]
+    public class UserController : ControllerBase
     {
+        private UserService userService;
+
+        public UserController(UserService _userService)
+        {
+            userService = _userService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterUserDto dto)
+        {
+            UserSummaryDto created = userService.RegisterUser(dto);
+
+            if (created == null)
+            {
+                return BadRequest(new { message = "Email is already registered." });
+            }
+
+            return Ok(created);
+        }
+
+
     }
 }
