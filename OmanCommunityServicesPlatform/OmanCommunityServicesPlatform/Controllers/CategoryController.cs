da7970234cccc85984076b3c2c09b5783e603bd7
Hidaya21 <alabrihidaya@gmail.com>
2026-07-27 16:49:33 +0400

    Add Category Controller

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Controllers/CategoryController.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Controllers/CategoryController.cs
@@ -1,6 +1,70 @@
-﻿namespace OmanCommunityServicesPlatform.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using OmanCommunityServicesPlatform.DTOs;
+using OmanCommunityServicesPlatform.Services;
+
+namespace OmanCommunityServicesPlatform.Controllers
 {
-    public class CategoryController
+    [ApiController]
+    [Route("category")]
+    public class CategoryController : ControllerBase
     {
+        private CategoryService categoryService;
+        public CategoryController(CategoryService _categoryService)
+        {
+            categoryService = _categoryService;
+        }
+        [HttpGet("GetAllCategories")]
+        public IActionResult GetAllCategories()
+        {
+            List<ResponseCategoryDTO> result = categoryService.GetAllCategories();
+
+            if (result.Count > 0)
+                return Ok(result);
+
+            return NoContent();
+        }
+
+        [HttpGet("GetCategoryById/{id}")]
+        public IActionResult GetCategoryById([FromRoute] int id)
+        {
+            ResponseCategoryDTO category = categoryService.GetById(id);
+
+            if (category == null)
+                return NotFound();
+
+            return Ok(category);
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add([FromBody] CreateCategoryDTO category)
+        {
+            ResponseCategoryDTO result = categoryService.Create(category);
+
+            if (result == null)
+                return BadRequest("Category name already exists.");
+
+            return Ok(result);
+        }
+
+        [HttpPut("Update/{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateCategoryDTO category)
+        {
+            ResponseCategoryDTO result = categoryService.Update(id, category);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+        [HttpDelete("Delete/{id}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            bool deleted = categoryService.Delete(id);
+
+            if (!deleted)
+                return NotFound();
+
+            return Ok("Category deleted successfully.");
+        }
     }
 }
