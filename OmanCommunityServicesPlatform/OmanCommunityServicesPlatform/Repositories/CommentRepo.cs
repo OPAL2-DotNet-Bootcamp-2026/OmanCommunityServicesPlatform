bea1a8e50c0cf921b3fb5cd374dd6f75d551a1d8
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-27 14:49:50 +0400

    Add CommentRepo for managing Comment entities

---
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Repositories/CommentRepo.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OmanCommunityServicesPlatform.Models;
+
+namespace OmanCommunityServicesPlatform.Repositories
+{
+    public class CommentRepo
+    {
+        private readonly OCSPContext context;
+
+        public CommentRepo(OCSPContext _context)
+        {
+            context = _context;
+        }
+
+        public List<Comment> GetByIssueId(int issueId)
+        {
+            return context.Comments
+                .Include(comment => comment.user)
+                .Where(comment => comment.issueId == issueId)
+                .OrderBy(comment => comment.commentDate)
+                .ToList();
+        }
+
+        public Comment? GetById(int commentId)
+        {
+            return context.Comments
+                .Include(comment => comment.user)
+                .FirstOrDefault(comment => comment.commentId == commentId);
+        }
+
+        public void Add(Comment comment)
+        {
+            context.Comments.Add(comment);
+            context.SaveChanges();
+        }
+
+        public void Delete(Comment comment)
+        {
+            context.Comments.Remove(comment);
+            context.SaveChanges();
+        }
+    }
+}
