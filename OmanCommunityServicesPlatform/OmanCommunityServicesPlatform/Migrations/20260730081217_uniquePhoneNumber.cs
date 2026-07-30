da7e5e09fd4ee4e95b82b47610a96aeff89bbe79
Mohammed Al Abri <striker.m901@gmail.com>
2026-07-30 12:13:30 +0400

    Enforce unique non-null phone numbers in Users table

---
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Migrations/20260730081217_uniquePhoneNumber.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OmanCommunityServicesPlatform.Migrations
+{
+    /// <inheritdoc />
+    public partial class uniquePhoneNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_phoneNumber",
+                table: "Users",
+                column: "phoneNumber",
+                unique: true,
+                filter: "[phoneNumber] IS NOT NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_phoneNumber",
+                table: "Users");
+        }
+    }
+}
