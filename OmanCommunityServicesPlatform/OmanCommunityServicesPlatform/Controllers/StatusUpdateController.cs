9da0f46135e85eb3dac606cb8b4622a67c2081f9
wajdan <wejdan95.almamari@gmail.com>
2026-09-16 16:03:20 +0400

    Add using directive for OmanCommunityServicesPlatform

--- OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Controllers/StatusUpdateController.cs
+++ OmanCommunityServicesPlatform/OmanCommunityServicesPlatform/Controllers/StatusUpdateController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OmanCommunityServicesPlatform.Services;
-
+using OmanCommunityServicesPlatform;
 namespace OmanCommunityServicesPlatform.Controllers
 {
     [ApiController]
